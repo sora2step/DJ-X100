e0a796b64d0d2975824866508cdd20cea216acb3
sora2step <68496660+sora2step@users.noreply.github.com>
2023-05-19 04:01:44 +0900

    Update x100cmdImport.bat

--- x100cmdImport.bat
+++ x100cmdImport.bat
@@ -1,8 +1,15 @@
-x100cmd check
-pause
 chcp 65001
+
+x100cmd check
+
+@echo:
+@echo  X100のCOMポートを確認してください。何かキーを押すとインポートを開始します。
+@pause > nul
 
 x100cmd import X100new.csv
 
-@pause
+@echo:
+@echo  インポートを完了しました、何かキーを押すとX100を再起動します。
+@pause > nul
+x100cmd exec restart
 exit
